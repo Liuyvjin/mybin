30338d8939f93ae916b8e894ff05a0e96837dca5
liuyvjin <lj1256289124@gmail.com>
2019-04-23 00:14:41 +0800

    修改zsh和bash的配置文件

--- mysetup.zsh
+++ mysetup.zsh
@@ -1,17 +1,19 @@
-#! bin/bash
-#bash
-#source /opt/ros/indigo/setup.bash  # 自动载入ros
-#source  ~/catkin_ws/devel/setup.bash  # 加载ros默认工作空间
-#zsh
+#! bin/zsh
+#将该文件夹下的路径加入环境变量mytypora
+PATH=$PATH:/home/liujin/mybin/mytypora
+
+#一些命令的别名
+alias rm='saferm.sh -q' #用这个sh代替rm，可以给rm加个垃圾桶 -q代表静默
+
+#ros
 source /opt/ros/indigo/setup.zsh  # 自动载入ros
 source  ~/catkin_ws/devel/setup.zsh  # 加载ros默认工作空间
-
-# 创建ros包
-alias ccpkg='catkin_create_pkg'
+alias ccpkg='catkin_create_pkg' # 简短的ros创建包指令
 
 # 配置clion
 export PATH=~/app/clion-2019.1/bin:${PATH}  
 # 定义opencv要用的环境变量
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig  
+
 
 
--- ubuntuinit.sh
+++ ubuntuinit.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # 这是重装系统后要运行一次的，用于将我的可执行文件目录添加到bashrc中
-echo "123123" |sudo -S sh -c 'echo "PATH=$PATH:/home/liujin/mybin\nsource /home/liujin/mybin/mysetup.sh" >> ~/.bashrc'
+echo "123123" |sudo -S sh -c 'echo "PATH=\$PATH:/home/liujin/mybin\nsource /home/liujin/mybin/mysetup.sh" >> ~/.bashrc'
 source ~/.bashrc
 
 #将安全删除脚本安装
