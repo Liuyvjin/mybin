85620bc0c6cd1b22313228b76498ceb09b773a89
liuyvjin <lj1256289124@gmail.com>
2019-05-22 14:35:41 +0800

    将ros的命令全部改成带参数的alias放在mysetup.zsh中

--- mysetup.zsh
+++ mysetup.zsh
@@ -2,14 +2,20 @@
 #将该文件夹下的路径加入环境变量mytypora
 PATH=$PATH:/home/liujin/mybin/mytypora
 
-#一些命令的别名
+# 一些命令的别名
 alias rm='saferm.sh -q' #用这个sh代替rm，可以给rm加个垃圾桶 -q代表静默
 
-#ros
+# ros
 source /opt/ros/indigo/setup.zsh  # 自动载入ros
 ##source  ~/work/ros_ws/devel/setup.zsh  # 加载ros默认工作空间
 source ~/work/ros_demo/devel/setup.zsh
 alias ccpkg='catkin_create_pkg' # 简短的ros创建包指令
+## 快捷载入ros工作空间
+alias loadros='func(){source ~/work/$1/devel/setup.zsh;echo 成功载入工作空间$1; };func'
+## 快捷打开moveit
+alias mymoveit='roslaunch moveit_setup_assistant setup_assistant.launch'
+## 编译一个指定的包cmpkg
+alias cmpkg='func(){catkin_make -DCATKIN_WHITELIST_PACKAGES="$1";};func'
 
 # 配置clion
 export PATH=~/app/clion-2019.1/bin:${PATH}  
